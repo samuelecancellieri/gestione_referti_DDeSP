dd32cd0160cb37415a92a14de381c5f7a7e85365
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-01-22 15:04:03 +0100

    update to pdf accett

--- stampa_pdf.py
+++ stampa_pdf.py
@@ -11,14 +11,15 @@ pdf.add_page()
 pdf.set_xy(0, 0)
 # set color and for grey titles
 pdf.set_font('arial', 'I', 12)
-pdf.set_text_color(220, 220, 220)
+pdf.set_text_color(220, 220, 220)  # grey
 pdf.cell(200, 10, "Struttura", 0, 0, 'C')
 pdf.ln(5)
+pdf.cell(-10)
 # set color and for for main text
-pdf.set_text_color(0, 0, 0)
+pdf.set_text_color(0, 0, 0)  # black
 pdf.set_font('arial', 'B', 12)
 pdf.cell(200, 10, "Laboratorio di Igiene", 0, 2, 'C')
-# pdf.cell(10)
+pdf.cell(10)
 pdf.cell(50, 10, 'Numero Modulo', 1, 0, 'C')
 pdf.cell(100, 10, '', 1, 1, 'C')
 pdf.cell(50, 10, 'Unità Operativa', 1, 0, 'C')
