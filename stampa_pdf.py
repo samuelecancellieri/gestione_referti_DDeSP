dfe27216298d4c4fbb1c9f2d128374afdbf01553
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-05-25 11:18:10 +0200

    test mr47

--- stampa_pdf.py
+++ stampa_pdf.py
@@ -343,9 +343,9 @@ def stampa_referto(codice_MR,id_accettazione, id_campione, unita_operativa, data
         pdf.set_font('arial', '', 8)
         pdf.cell(0, 10, 'n.r.*: non rivelato, nessun sviluppo o < 1 UFC', 0, 0, 'L')
         # manually writing of multi row text in cell
-        pdf.text(135,115,'Allegato 4 Dossier 133/2006')
-        pdf.text(127,131,'"Reprocessing degli endoscopi')
-        pdf.text(127,134,' - indicazioni operative"')
+        pdf.text(140,115,'Allegato 4 Dossier 133/2006')
+        pdf.text(140,119,'"Reprocessing degli endoscopi')
+        pdf.text(140,123,' - indicazioni operative"')
         # nota post tabella classe
         pdf.set_left_margin(10)
         #skip line of #mm
