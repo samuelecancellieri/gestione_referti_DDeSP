aca9dccf3916f83253c403ab097217e8d783e674
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-05-25 11:04:14 +0200

    update xy for piè di pagina

--- stampa_pdf.py
+++ stampa_pdf.py
@@ -109,7 +109,6 @@ def stampa_referto_identificazione(id_accettazione, id_campione, unita_operativa
     pdf.cell(60, 10, '* Mediante VITEK® 2 Compact, bioMérieux')
 
     # nota post tabella classe
-    # pdf.set_left_margin(10)
     pdf.set_xy(10,220)
     pdf.set_font('arial', '', 10)
     pdf.cell(0, 0, '', 0, 1, 'C')
@@ -129,7 +128,7 @@ def stampa_referto_identificazione(id_accettazione, id_campione, unita_operativa
     pdf.set_font('arial', 'I', 10)
     pdf.cell(
         0, 10, "________________________________________________________________________________________________", 0, 1, 'L')
-    pdf.cell(0, 10, 'MR 32 rev. 1 - Pag 1 a 1', 0, 1, 'R')
+    pdf.cell(0, 10, 'MR 49 rev. 1 - Pag 1 a 1', 0, 1, 'R')
     pdf.set_font('arial', '', 10)
     pdf.cell(0, 10, 'Laboratorio di Igiene', 0, 0, 'L')
     pdf.ln(4)
@@ -369,8 +368,6 @@ def stampa_referto(codice_MR,id_accettazione, id_campione, unita_operativa, data
         
     # nota post tabella classe
     pdf.set_xy(10,220)
-    # pdf.ln(20)
-    # pdf.set_left_margin(10)
     pdf.cell(
         0, 10, 'prof. Stefano Tardivo', 0, 0, 'R')
     pdf.ln(3)
@@ -492,8 +489,7 @@ def stampa_accettazione(numero_modulo, unita_operativa, data_prelievo, data_acce
             pdf.cell(60, 10, '', 1, 1, 'C')
 
     # firma e terminazione documento
-    # pdf.ln(5)
-    pdf.set_xy(10,220)
+    pdf.ln(5)
     pdf.set_text_color(0, 0, 0)  # black
     pdf.set_font('arial', '', 12)
     pdf.cell(0, 10, "Note:", 0, 1, 'L')
